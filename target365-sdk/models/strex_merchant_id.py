1e5fae1f3d910e10bae36de77230d905989f1388
Brando-Programmo <38990902+Brando-Programmo@users.noreply.github.com>
2018-10-10 17:30:17 -0600

    rename encryptedPassword to password (bug)

--- target365-sdk/models/strex_merchant_id.py
+++ target365-sdk/models/strex_merchant_id.py
@@ -2,4 +2,4 @@ class StrexMerchantId:
     def fromDict(self, dict):
         self.merchantId = dict["merchantId"]
         self.shortNumberId = dict["shortNumberId"]
-        self.encryptedPassword = dict["encryptedPassword"]
+        self.password = dict["password"]

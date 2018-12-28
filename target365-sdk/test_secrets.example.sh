678aa5cd84657543ea3e6283eff97daf002c8e84
Sam <sam@expertcoder.io>
2018-12-28 16:41:58 +1100

    API secrets needed for the automated tests now come from environmental variables set in these scripts.

---
+++ target365-sdk/test_secrets.example.sh
@@ -0,0 +1,6 @@
+# To run automated tests make sure there is a test_secrets.sh file which
+# exports the following environmental variables
+
+export API_KEY_NAME=PythonSdkTest
+export API_PRIVATE_KEY=27683a52a4d08074a87da02255c9c4dd37a1b106229890c13e630d156ef89060
+# The API Private key should be a 32 character hex string. The value provided is only an example

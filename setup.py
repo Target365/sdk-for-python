8ac9f9ba91f3f3432b89074b69a2b5f353348994
HansOlavS <hans.olav.stjernholm@outlook.com>
2020-08-30 19:34:51 +0200

    Updated version to 1.5.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="target365_sdk",
-    version="1.5.0",
+    version="1.5.1",
     author="Target365",
     author_email="support@target365.no",
     description="Target365 SDK",
